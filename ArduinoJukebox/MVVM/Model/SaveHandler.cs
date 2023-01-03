a0469cae1a066d67f21b4d169f162b47bc9d80f4
Meyn <LAPTOP>
2023-01-03 18:33:49 +0100

    Projektdateien hinzufügen.

---
+++ ArduinoJukebox/MVVM/Model/SaveHandler.cs
@@ -0,0 +1,66 @@
+﻿using ArduinoJukebox.MVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace ArduinoJukebox.MVVM.Model
+{
+    internal static class SaveHandler
+    {
+        private static SettingsViewModel? SettingsVM => (MainViewModel.Instance?.VMs[1] as SettingsViewModel);
+        private static HomeViewModel? HomeVM => (MainViewModel.Instance?.VMs[0] as HomeViewModel);
+
+        public static DeviceInfo? LastConnectedDevice { get; set; }
+
+        /// <summary>
+        /// Save song path file and last used ble device
+        /// </summary>
+        public static void Save()
+        {
+            if (SettingsVM != null)
+            {
+
+
+            }
+        }
+
+        /// <summary>
+        /// load song path file and last used ble device
+        /// </summary>
+        public static void Load()
+        {
+            if (SettingsVM != null)
+            {
+
+            }
+        }
+
+
+        public static void LoadSongs()
+        {
+            if (SettingsVM != null && HomeVM != null)
+            {
+                try
+                {
+                    string[] files = Directory.GetFiles(SettingsVM.LibraryPath, "*.song8");
+                    List<Song> songs = new(files.Length);
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        Song? song = JsonSerializer.Deserialize<Song>(File.ReadAllText(files[i]), new JsonSerializerOptions() { WriteIndented = true });
+                        if (song != null)
+                            songs.Add(song);
+                    }
+                    HomeVM.Songs = new(songs);
+                    HomeVM.ActualSong = songs.FirstOrDefault();
+                }
+                catch (Exception)
+                {
+
+                }
+
+            }
+        }
+    }
+}
---
+++ ArduinoJukebox/MVVM/View/WindowControlButtons.xaml.cs
@@ -0,0 +1,39 @@
+﻿using ArduinoJukebox.Core;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ArduinoJukebox.MVVM.View
+{
+    /// <summary>
+    /// Interaktionslogik für WindowControlButtons.xaml
+    /// </summary>
+    public partial class WindowControlButtons : UserControl
+    {
+        public WindowControlButtons()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Commamd that Close the window
+        /// </summary>
+        public RelayCommand CloseCommand { get; } =
+        new RelayCommand(o =>
+        {
+            ((Window)o).Close();
+        });
+
+        /// <summary>
+        /// Command that Minimize the window
+        /// </summary>
+        public RelayCommand MinimizeCommand { get; } =
+        new RelayCommand(o => ((Window)o).WindowState = WindowState.Minimized);
+
+        /// <summary>
+        /// Command that Maximize the window
+        /// </summary>
+        public RelayCommand MaximizeCommand { get; } =
+      new RelayCommand(o => { Window w = (Window)o; w.WindowState = w.WindowState != WindowState.Maximized ? WindowState.Maximized : WindowState.Normal; });
+
+    }
+}
